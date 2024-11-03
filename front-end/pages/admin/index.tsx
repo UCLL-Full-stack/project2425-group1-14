11e6763b6998e1bb915a94decdfc03164e439365
Jackvanhecke <jack.vanhecke@student.ucll.be>
2024-11-03 12:26:01 +0100

    voter and admin page

---
+++ front-end/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the admin panel. Here you can manage users, view reports, and configure settings.</p>
+
+        </div>
+    );
+};
+
+export default AdminPage;
