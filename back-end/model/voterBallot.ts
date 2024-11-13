9c011a161cd04daba633ed23224748f8d73c8ba0
~julia <uesugi.sapero@gmail.com>
2024-11-13 17:02:11 +0100

    prisma pre format

--- back-end/model/voterBallot.ts
+++ back-end/model/voterBallot.ts
@@ -4,27 +4,27 @@ import {Voter} from "./voter";
 export class VoterBallot {
     private ballot: Ballot;
     private voter: Voter;
-    private votes: string[];
+    private votedFor: String;
 
-    constructor(voterBallot: {ballot: Ballot; voter: Voter; votes: string[]}) {
+    constructor(voterBallot: {ballot: Ballot; voter: Voter; votedFor: String}) {
         this.ballot = voterBallot.ballot;
         this.voter = voterBallot.voter;
-        this.votes = voterBallot.votes;
+        this.votedFor = voterBallot.votedFor;
     }
 
     getBallot(): Ballot { return this.ballot; }
     getVoter(): Voter { return this.voter; }
-    getVotes(): string[] { return this.votes; }
+    getVotes(): String { return this.votedFor; }
 
-    changeVotes(votes: string[]): void {
-        this.votes = votes;
+    changeVotes(votedFor: String): void {
+        this.votedFor = votedFor;
     }
 
     equals(voterBallot: VoterBallot): boolean {
         return (
             this.ballot === voterBallot.getBallot() &&
             this.voter === voterBallot.getVoter() &&
-            voterBallot.getVotes().every((vote) => {this.votes.includes(vote)})
+            this.votedFor === voterBallot.getVotes()
         );
     }
 }
