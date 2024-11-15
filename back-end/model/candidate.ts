85ec93e259b8ff87fe53c73e899fc12d944b8e0d
~julia <uesugi.sapero@gmail.com>
2024-11-15 03:52:42 +0100

    candidate, seed

--- back-end/model/candidate.ts
+++ back-end/model/candidate.ts
@@ -5,26 +5,22 @@ import { Type } from "./type";
 export class Candidate {
     readonly  id?: number;
     readonly  name: string;
-    readonly  image: string;
     readonly  region: Region;
 
-    constructor(candidate: { name: string; image: string; region: Region; parties: PartyCandidate[]; id?: number }) {
+    constructor(candidate: { name: string; region: Region; parties: PartyCandidate[]; id?: number }) {
         this.id = candidate.id;
         this.name = candidate.name;
-        this.image = candidate.image;
         this.region = candidate.region;
     }
 
     getId(): number | undefined { return this.id; }
     getName(): string { return this.name; }
-    getImage(): string { return this.image; }
     getRegion(): Region { return this.region; }
     
     equals(candidate: Candidate): boolean {
         return (
             this.id === candidate.getId() &&
             this.name === candidate.getName() &&
-            this.image === candidate.getImage() &&
             this.region === candidate.getRegion()
         );
     }
