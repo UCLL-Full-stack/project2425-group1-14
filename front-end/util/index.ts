f1450b3259659fa2ab842048b286db3beb38c22d
~julia <uesugi.sapero@gmail.com>
2024-12-10 22:02:18 +0100

    authenticated request util

---
+++ front-end/util/index.ts
@@ -0,0 +1,27 @@
+async function makeAGR(url: string, method: string, body?: string) { // authenticated get request
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    });
+
+    return await response.json();
+}
+
+async function makeAPR(url: string, method: string, body: Object) { // authenticated post request
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+		body: JSON.stringify(body)
+    });
+
+    return await response.json();
+}
+
+export {
+    makeAGR,
+    makeAPR,
+}
